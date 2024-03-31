023a14a5f85500cad8bd4bb5cab04857935b1a8e
Adlet Taigulov <adlettaigulov@gmail.com>
2024-03-31 19:47:43 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./styles/index.scss";
+import { Route, Routes, Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
+import { useTheme } from "./styles/theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames("app", {}, [])}>
+      <button onClick={toggleTheme}>Toggle</button>
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О нас</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
