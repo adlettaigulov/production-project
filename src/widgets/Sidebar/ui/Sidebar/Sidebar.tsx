db70842952a39e6bc4011c5c3bccb3774e1690c7
Adlet Taigulov <adlettaigulov@gmail.com>
2024-04-07 00:21:24 +0500

    add LanguageSwitcher component, add i18n plugin, add translates

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
 import { useState } from "react";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher/ui";
+import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LanguageSwitcher } from "shared/ui/LanguageSwitcher";
 interface SidebarProps {
   className?: string;
 }
@@ -22,6 +23,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <button onClick={onToggle}>Toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
+        <LanguageSwitcher className={cls.lang} />
       </div>
     </div>
   );
